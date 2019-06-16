221cdd82b2e5c6565c857fdf59bd5799989ab9fa
lulumbp15 <luoweizhou23@163.com>
2019-06-16 15:04:20 +0800

    Unity Initialized
    Import the old freehand kit package

---
+++ FreehandKit/Assets/LeapMotionVRShopping/LPVRShopping/Scripts/ObjectMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMenuManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NextPanel() { }
+    public void JumpToPanel(int m_index)
+    {
+
+    }
+
+
+}
