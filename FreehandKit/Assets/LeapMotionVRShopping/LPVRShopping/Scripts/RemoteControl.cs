8cb50fe7ad81df568add1283c78453c3a2b06896
lulumbp15 <luoweizhou23@163.com>
2019-06-17 16:33:50 +0800

    RemoteControl
    Finished RemoteControl as the hub for all functionality controls

--- FreehandKit/Assets/LeapMotionVRShopping/LPVRShopping/Scripts/RemoteControl.cs
+++ FreehandKit/Assets/LeapMotionVRShopping/LPVRShopping/Scripts/RemoteControl.cs
@@ -7,17 +7,112 @@ public interface ICommand
     void execute();
 }
 
+public class NoCommand : ICommand
+{
+    public void execute() { }
+}
+
 public class RemoteControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject m_gameObject;
+    private Vector3 originalScale;
+    private ICommand command = new NoCommand();
+
     void Start()
     {
-
+        DetectionManager.Get().getCurrentObject += GetCurrentObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetCurrentObject(GameObject currentObject)
     {
-
+        if (currentObject != null)
+        {
+            this.m_gameObject = currentObject;
+            this.originalScale = m_gameObject.transform.localScale;
+        }
     }
+
+    //MENU COMMAND
+    public void OpenMenuCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new OpenMenuCommand(m_gameObject);
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    public void CloseMenuCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new CloseMenuCommand(m_gameObject);
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    //COLOR COMMAND
+    public void RedColorCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new ColorBehaviourCommand(m_gameObject, new RedColor());
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    public void BlueColorCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new ColorBehaviourCommand(m_gameObject, new BlueColor());
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    public void GreenColorCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new ColorBehaviourCommand(m_gameObject, new GreenColor());
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    //SIZE COMMAND
+    public void OriginalSizeCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new SizeBehaviourCommand(m_gameObject, originalScale, new OriginalSize());
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    public void SmallSizeCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new SizeBehaviourCommand(m_gameObject, originalScale, new SmallSize());
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
+    public void LargeSizeCommand()
+    {
+        if (m_gameObject != null)
+        {
+            command = new SizeBehaviourCommand(m_gameObject, originalScale, new LargeSize());
+            command.execute();
+        }
+        else { Debug.Log("WE DONOT HAVE A OBJECT."); }
+    }
+
 }
